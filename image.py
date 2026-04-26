20dbee84fe40ff6d57a462e936bca3026e2e884c
Isaac David <orpheezt@outlook.com>
2026-04-26 18:32:20 -0500

    Completed Taller 9

--- image.py
+++ image.py
@@ -2,7 +2,7 @@ import modal
 
 image = (
     modal.Image.from_registry("nvidia/cuda:13.2.1-devel-ubuntu24.04", add_python="3.14")
-    .apt_install("libxcb1")
+    .apt_install(["libxcb1", "libgl1", "libglib2.0-0"])
     .uv_pip_install(
         "torch>=2.11",
         "torchvision",
@@ -10,7 +10,9 @@ image = (
         "pillow",
         "pandas",
         "matplotlib",
-        "kaggle",
+        "kagglehub",
+        "ipykernel",
+        "ipywidgets",
     )
 )
 
